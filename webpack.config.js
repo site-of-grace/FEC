a076f2ec394284ef59db5b2702bf9b415e0d8feb
Daniel Avila <messagedaniel@protonmail.com>
2023-03-04 16:29:10 -0500

    add eslint to project

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,21 @@ var SRC_DIR = path.join(__dirname, '/src');
 var DIST_DIR = path.join(__dirname, '/public/dist');
 
 module.exports = {
-	mode: 'development',
-	entry: `${SRC_DIR}/index.js`,
-	output: {
-		filename: 'bundle.js',
-		path: DIST_DIR,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				}
-			}
-		]
-	}
+  mode: 'development',
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
