be01dd85d39ac67fee54aaec54b3c5f12ba8dbfd
Stanley Chu <chustanleys@gmail.com>
2023-03-06 12:44:08 -0800

    Created skeleton for FEC

---
+++ src/components/relatedItems/relatedItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RelatedItems = () => {
+
+
+  return (
+    <div className='widget'>
+      <h1>RELATED ITEMS</h1>
+    </div>
+  )
+}
+
+export default RelatedItems;
