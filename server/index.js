af14bdf6503c76f9261c444defa505abdf438880
BradySBaker <bradysamuelbaker@gmail.com>
2023-03-08 12:34:09 -0800

    index.js - Connected to API for reviews, added get request /reviews

--- server/index.js
+++ server/index.js
@@ -4,10 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 var axios = require('axios');
-// var githubToken = require('../githubToken.js');
 var githubToken = process.env.GITHUB_TOKEN;
 
-// app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(__dirname + '/../public/dist'));
 app.use(express.json());
 
@@ -32,6 +30,50 @@ app.get('/FEC', (req, res) => {
     });
 });
 
+let config = {
+  headers: {
+    Authorization: `${githubToken}`
+  }
+}
+
+
+//--Api request for reviews I know stanley planned on using the one above for everything but don't wanna cause merge conflict
+app.get('/reviews', (req, res) => {
+  var sendError = () => {
+    res.statusCode = 404;
+    res.end();
+  };
+
+  //current product ids = 71697, 71698, 71699, 71700, 71701
+  let config = {
+    headers: {
+      Authorization: `${githubToken}`
+    }
+  };
+  //Sends 2 reviews at a time
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews/?product_id=71701', config)
+  .then((apiData) => {
+    if (!apiData) {
+      sendError();
+      throw apiData;
+    }
+    if (apiData.data.results) {
+      var results = apiData.data.results;
+      console.log('---review data--->', results);
+      res.statusCode = 200;
+      res.send(JSON.stringify(results));
+    } else {
+      sendError();
+    }
+
+  })
+  .catch((error) => {
+    console.log('---server error--->', error);
+    sendError();
+  })
+})
+
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
 });
+
