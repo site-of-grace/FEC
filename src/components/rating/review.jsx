4985211d6b63cd51b1529f69b2874bf6a9055c64
BradySBaker <bradysamuelbaker@gmail.com>
2023-03-08 18:34:47 -0800

    Added image selector for individual icons

--- src/components/rating/review.jsx
+++ src/components/rating/review.jsx
@@ -2,24 +2,26 @@ import React, {useState} from 'react';
 
 const Review = ({review}) => {
 	const [expand, setExpand] = useState(false);
+	const [selectedImg, setSelectImg] = useState(false);
 
 	var stars = [];
 	var date = new Date(review.date);
 	var date = date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
 
-	review.body = 'Review Body - Any images that were submitted as part of the review should appear as thumbnails below the review text. Upon clicking a thumbnail, the image should open in a modal window, displaying at full resolution.  The only functionality available within this modal should be the ability to close the window.';
-
 	for (var i = 0; i < 5; i++) {
 		if (i <= review.rating) {
-			stars.push(<img key={i} src='./icons/fullStar.png'></img>)
+			stars.push(<img className='review-star' key={i} src='./icons/fullStar.png'></img>)
 		} else {
-			stars.push(<img key={i} src='./icons/unfilledStar.png'></img>);
+			stars.push(<img className='review-star' key={i} src='./icons/unfilledStar.png'></img>);
 		}
 	}
 
-
 	return (
 		<div className='review'>
+			{selectedImg ? <div id='review-imgModel'>
+				<img className={'review-selectedImg'} src={selectedImg}></img>
+				<button onClick={() => setSelectImg(false)} style={{'color': 'red', 'fontSize': '50px'}}>X</button>
+			</div> : null}
 			{stars}
 			<div className='review-date'>{date}</div>
 			<div className='review-summary'>{review.summary}</div>
@@ -28,6 +30,9 @@ const Review = ({review}) => {
 				{!expand && review.body.length > 250 ? '...' : ''}
 				{!expand && review.body.length > 250 ? <button onClick={() => setExpand(true)}style={{'fontSize': '5px'}}>Show more</button> : null}
 			</div>
+			{review.photos.map((curPhoto) => {
+				return <img onClick={() => setSelectImg(curPhoto.url)} className='review-photo' src={curPhoto.url} key={curPhoto.id}></img>
+			})}
 		</div>
 	)
 }
