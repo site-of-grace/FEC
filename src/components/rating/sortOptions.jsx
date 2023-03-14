18d83423846c3ac531f25d853cd706e5c6894280
BradySBaker <bradysamuelbaker@gmail.com>
2023-03-14 16:14:14 -0700

    Added sort picker with options

--- src/components/rating/sortOptions.jsx
+++ src/components/rating/sortOptions.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import {useSelector } from 'react-redux';
 var SortOptions = function() {
 	const reviews = useSelector((state) => state.rating.reviews); // review.slice
+
+	const handleSort = (e) => {
+		console.log(e.target.value);
+	};
+
 	return(
 		<div id='sort-bar'>
 			<div>{reviews.length} reviews, sorted by</div>
-			<select name='sort-options' id='sort-options'>
+			<select onChange={handleSort} name='sort-options' id='sort-options'>
 			<option value='relevance'>relevance</option>
 			<option value='helpful'>helpful</option>
 			<option value='recent'>recent</option>
