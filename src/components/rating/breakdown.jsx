1dd911a582d1c51acaa333a2c6ac9c163ec35a84
BradySBaker <bradysamuelbaker@gmail.com>
2023-03-18 10:38:42 -0700

    Added percent recommended

--- src/components/rating/breakdown.jsx
+++ src/components/rating/breakdown.jsx
@@ -6,14 +6,22 @@ import Stars from '../general/Stars.jsx';
 
 const Breakdown = () => {
 	const averageRating = useSelector((state) => state.rating.average);
+	const metaData = useSelector((state) => state.rating.ratingMeta);
+	console.log(metaData);
 
 	const starsDiv = Stars({number: averageRating});
-
+	var percentRecommended;
+	if (metaData.recommended) {
+		var recommended = metaData.recommended;
+		percentRecommended = Math.round((Number(recommended.true)/(Number(recommended.true) + Number(recommended.false)))*100);
+		console.log(percentRecommended);
+	}
 	return (
 		<div id='rating-breakdown'>
 			<div id='rating-breakdown-title'>RATINGS & REVIEWS</div>
 			<div id='rating-average'>{Math.round(averageRating*10)/10}</div> {/*Rounds to nearest single decimal*/}
 			<div id='rating-starsAverage'>{starsDiv}</div>
+			<div id='rating-recommended'>{percentRecommended}</div>
 		</div>
 	);
 };
