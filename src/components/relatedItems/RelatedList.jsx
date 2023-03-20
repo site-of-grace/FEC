38411a7d05311af175af1dff4d554b13586aa33a
Daniel Avila <messagedaniel@protonmail.com>
2023-03-20 15:43:19 -0400

    chevron and sliding carousel in progress

--- src/components/relatedItems/RelatedList.jsx
+++ src/components/relatedItems/RelatedList.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+import { ChevronRight } from 'lucide-react';
 import styles from './styles.module.css';
 import Card from './Card.jsx';
 
+
 const RelatedList = ({ products }) => {
   return (
-      <div className={styles.row}>
-        {products.map((product) => (
-          <Card
-            product={product}
-            key={product.id}
-          />
-        ))}
-      </div>
+    //   <Swiper
+    //   spaceBetween={10}
+    //   slidesPerView={6}
+    //   onSlideChange={() => console.log('slide change')}
+    //   onSwiper={(swiper) => console.log(swiper)}
+    //   className={styles.row}
+    //  >
+    // <SwiperSlide key={product.id}>
+    // <Card
+    //   product={product}
+    //   key={product.id}
+    // />
+    // </SwiperSlide>
+
+    <div className={styles.row}>
+      {products.map((product, i) => (
+        <>
+        <Card
+          product={product}
+          key={product.id}
+        />
+        {i === products.length - 1 && (
+          <>
+          <span className={styles.slider} />
+          <span className={styles['chevron-hover']} />
+          <ChevronRight className={styles['chevron-right']}/>
+          </>
+        )}
+        </>
+      ))}
+
+      {/* <div className={styles.overlayRow}>
+      <span className={styles.slider} />
+      </div> */}
+
+    </div>
+    // </Swiper>
   );
 };
 
