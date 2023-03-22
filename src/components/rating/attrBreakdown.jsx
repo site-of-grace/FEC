b6a8794b17a94ad9b12c028b943fdd15a7b415c7
BradySBaker <bradysamuelbaker@gmail.com>
2023-03-22 16:14:20 -0700

    Added 5 bars for comfor and quality, also arrow that follows rating

--- src/components/rating/attrBreakdown.jsx
+++ src/components/rating/attrBreakdown.jsx
@@ -8,22 +8,38 @@ const AtrributeBreakdown = () => {
 
 	var attributeDivs = [];
 	if (metaData.characteristics) {
-		var endSize = 240; //barSize - arrowSize
+		var endSize = 245; //barSize - arrowSize
 		var attributes = metaData.characteristics;
+		var barLables = {};
+		barLables.Comfort = ['Poor', 'Fair', 'Average', 'Good', 'Great'];
+		barLables.Quality = ['Poor', 'Fair', 'Average', 'Good', 'Great'];
+		barLables.Size = ['Too small', 'Perfect', 'Too large'];
+		barLables.Width = ['Too small', 'Perfect', 'Too large'];
+
 		for (var key in attributes) {
 
 			var barDivs = [];
-			var barLables = ['Too small', 'Perfect', 'Too large'];
-			for (var i = 0; i < 3; i++) {
+			var name = barLables[key];
+			for (var i = 0; i < name.length; i++) {
+				var textStyle = {'fontSize': '9px', 'marginTop':'4px', 'marginLeft': '12px'};
+				if (name[i] === 'Perfect') {
+					textStyle['marginLeft'] = '26px';
+				}
+				if (name[i] === 'Too small') {
+					textStyle['marginLeft'] = '0px';
+				}
+				if (name[i] === 'Too large') {
+					textStyle['marginLeft'] = '40px';
+				}
+
 				barDivs.push(<div className='rating-attrBarContainer' key={i*10} >
-					<div className='rating-attrBar'></div>
-					<div style={{'fontSize': '10px', 'marginTop':'4px'}}>{barLables[i]}</div>
+					{name.length === 3 ? <div className='rating-attrBar3'></div> : <div className='rating-attrBar5'></div>}
+					<div style={textStyle}>{name[i]}</div>
 				</div>);
 			}
 
 			var value = attributes[key].value;
-
-			var ratingPercent = value/6; //Using 6 since 3 is middle
+			var ratingPercent = (value-1)/4; //So a rating of 1 = 0% and a rating of 5 = 100%
 			var marginLeft = endSize * ratingPercent; //Puts arrow in correct spot
 
 			attributeDivs.push(<div className='rating-attrBarSection' key={attributes[key].id}>
