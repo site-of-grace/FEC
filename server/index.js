e79f8373574b9364fb45e45b16d5f2b0a3b4cf43
Matthew McIvor <matthewrmcivor@gmail.com>
2023-03-29 18:58:15 -0700

    added post route

--- server/index.js
+++ server/index.js
@@ -85,38 +85,56 @@ app.get('/questions', (req, res) => {
   var product_id = req.query.product_id;
   console.log('req.query====> ',req.query);
   // console.log('productId in /QA/questions==========> ', product_id);
-  axios
-    .get(`${api}/qa/questions/?product_id=${product_id}`, config)
-    .then((data) => {
-      if (!data) {
-        throw data;
-      }
-      console.log('OK request on questions route, data.data: ', data.data);
-      res.send(data.data);
-    })
-    .catch((error) => {
-      // console.log('error on questions route ', error);
-      res.send(error);
-    });
+  axios.get(`${api}/qa/questions/?product_id=${product_id}`, config)
+  .then((data) => {
+    if (!data) {
+      throw data;
+    }
+    console.log('OK request on questions route, data.data: ', data.data);
+    res.send(data.data);
+  })
+  .catch((error) => {
+    // console.log('error on questions route ', error);
+    res.send(error);
   });
-  
-  app.put('/answer/helpful/:answer_id', (req, res) => {
-    var answer_id = req.params.answer_id;
-    console.log('answer_id====> ', answer_id);
-    // console.log('req.params====> ', req.params);
-    // console.log('productId in /QA/questions==========> ', product_id);
-    axios
-      .put(`${api}/qa/answers/${answer_id}/helpful`, null, config)
-      .then((data) => {
-        res.sendStatus(204);
-        // console.log('data==========> ', data);
-        console.log('GOOD HELPFUL REQUEST');
-      })
-      .catch((error) => {
-        console.log('error on questions helpful route ', error);
-        res.send(error);
-      });
-    });
+});
+
+app.put('/answer/helpful/:answer_id', (req, res) => {
+  var answer_id = req.params.answer_id;
+  console.log('answer_id====> ', answer_id);
+  // console.log('req.params====> ', req.params);
+  // console.log('productId in /QA/questions==========> ', product_id);
+  axios.put(`${api}/qa/answers/${answer_id}/helpful`, null, config)
+  .then((data) => {
+    res.sendStatus(204);
+    // console.log('data==========> ', data);
+    console.log('GOOD HELPFUL REQUEST');
+  })
+  .catch((error) => {
+    console.log('error on questions helpful route ', error);
+    res.send(error);
+  });
+});
+
+app.post('/qa/questions', (req, res) => {
+  const { body, name, email, product_id } = req.body;
+  // console.log('body, name, email, product_id, config====>  ', body, name, email, product_id, config);
+  // console.log('req.body===', req.body);
+  axios.post(`${api}/qa/questions`, { body, name, email, product_id }, config)
+  .then((data) => {
+    if (!data) {
+      throw data;
+    }
+    console.log('OK request on questions route. data.data===> ', data.data);
+    res.sendStatus(201);
+  })
+  .catch((error) => {
+    // console.log('error on questions route ', error);
+    res.send(error);
+  });
+});
+
+
 // ===================================================
 
 
