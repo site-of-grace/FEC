1086fa6748af0950a5e1456fd1e54ed5dd3fa394
Stanley Chu <chustanleys@gmail.com>
2023-04-05 14:03:05 -0700

    compressed bundle.js in webpack and added cart picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import Overview from './components/overview/overview.jsx';
 import Rating from './components/rating/rating.jsx';
 import Questions from './components/questions/questions.jsx';
 import RelatedItems from './components/relatedItems/relatedItems.jsx';
+import { BsFillCartFill } from 'react-icons/bs';
 
 const App = () => {
 
+  const [cart, setCart] = useState(0);
 
   return (
   <>
     <header id='banner'>
-      <h1 id='bannerTitle'>GRACE OUTFITTERS</h1>
+      <h1 id='bannerTitle' onClick={()=>{console.log(cart)}}>GRACE OUTFITTERS</h1>
+      <div id='cartIcon'>
+        <BsFillCartFill size={'30'} color={'white'}/>
+        <div id='cartCount'>
+          <>{cart}</>
+        </div>
+      </div>
     </header>
      <div id='bannerMessage'>
       SITE-WIDE ANNOUNCEMENT MESSAGE! - SALE / DISCOUNT&nbsp;<b>OFFER</b>&nbsp;-&nbsp;<div style={{'text-decoration': 'underline'}}> NEW PRODUCT HIGHLIGHT</div></div>
     <div id='FEC'>
-      <Overview />
+      <Overview setCart={setCart} cart={cart}/>
       <RelatedItems />
       <Questions />
       <Rating />
